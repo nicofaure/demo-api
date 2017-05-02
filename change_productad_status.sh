30e9ac45baf1413715b3abb2621d779a70d9a5d7
nicofaure <nicolas.faure@mercadolibre.com>
2017-05-02 19:43:37 -0300

    Change product status

---
+++ change_productad_status.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source access_token.sh
+
+curl -X PUT -H "Content-Type: application/json" "https://api.mercadolibre.com/advertising/product_ads/ads/$ITEM_ID?access_token=$TOKEN" -d '{"status": "paused"}'
