112d133d8c3be57a4897362b7255aa49a3801a01
nicofaure <nicolas.faure@mercadolibre.com>
2017-05-03 09:26:29 -0300

    Refresh token

--- edit_campaign.sh
+++ edit_campaign.sh
@@ -2,4 +2,4 @@
 
 source access_token.sh
 
-curl -X PUT -H "Content-Type: application/json" "https://api.mercadolibre.com/advertising/product_ads/campaigns/$DEFAULT_CAMPAIGN_ID?access_token=$TOKEN" -d '{"sync_new_listings": "disabled", "status": "active", "plan_id": 109}'
+curl -X PUT -H "Content-Type: application/json" "https://api.mercadolibre.com/advertising/product_ads/campaigns/$DEFAULT_CAMPAIGN_ID?access_token=$TOKEN" -d '{"sync_new_listings": "disabled", "status": "active", "plan_id": 109}' | jq .
--- get_productad.sh
+++ get_productad.sh
@@ -2,4 +2,4 @@
 
 source access_token.sh
 
-curl "https://api.mercadolibre.com/advertising/product_ads/ads/$ITEM_ID?access_token=$TOKEN"
+curl "https://api.mercadolibre.com/advertising/product_ads/ads/$ITEM_ID?access_token=$TOKEN" | jq .
